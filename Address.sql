74ee10ecf04dc443559449b89ffa0f4cf1348d24
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2023-04-18 23:23:23 +0700

    Reformat Code

--- Address.sql
+++ Address.sql
@@ -5,9 +5,9 @@ CREATE TABLE sbd.Address
     house_number INT          NOT NULL,
     rt           INT          NOT NULL,
     rw           INT          NOT NULL,
-    village VARCHAR(255) NOT NULL ,
-    district VARCHAR(255) NOT NULL ,
-    city VARCHAR(255) NOT NULL ,
-    province VARCHAR(255) NOT NULL ,
-    postal_code VARCHAR(5) NOT NULL
+    village      VARCHAR(255) NOT NULL,
+    district     VARCHAR(255) NOT NULL,
+    city         VARCHAR(255) NOT NULL,
+    province     VARCHAR(255) NOT NULL,
+    postal_code  VARCHAR(5)   NOT NULL
 );
