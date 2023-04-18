53dd204730c7e6f93c83ca373918a38d2eb3df06
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2023-04-19 00:31:36 +0700

    Add Agency Entity with Its Relationship

---
+++ Agency.sql
@@ -0,0 +1,23 @@
+CREATE TABLE sbd.agency
+(
+    id           VARCHAR(36) PRIMARY KEY,
+    address_id   VARCHAR(36),
+    first_name   VARCHAR(255),
+    last_name    VARCHAR(255),
+    email        VARCHAR(255),
+    password     VARCHAR(255),
+    phone_number VARCHAR(15),
+    gender       ENUM ('MALE', 'FEMALE'),
+    age          INT,
+    FOREIGN KEY (address_id) REFERENCES sbd.address (id)
+);
+
+ALTER TABLE sbd.agency
+    ALTER id SET DEFAULT (UUID()),
+    MODIFY first_name VARCHAR(255) NOT NULL,
+    MODIFY last_name VARCHAR(255) NOT NULL,
+    MODIFY email VARCHAR(255) NOT NULL,
+    MODIFY password VARCHAR(255) NOT NULL,
+    MODIFY phone_number VARCHAR(15) NOT NULL,
+    MODIFY gender ENUM ('MALE', 'FEMALE') NOT NULL,
+    MODIFY age INT NOT NULL;
