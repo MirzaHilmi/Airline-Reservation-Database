4e99fbf271a7250ce01f27c5d766ba6848e0add9
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2023-04-19 00:13:03 +0700

    Separate Constraint to its own Line of Query

--- Address.sql
+++ Address.sql
@@ -1,13 +1,25 @@
-CREATE TABLE sbd.Address
+CREATE TABLE sbd.address
 (
-    id           VARCHAR(36) DEFAULT (UUID()),
-    street       VARCHAR(255) NOT NULL,
-    house_number INT          NOT NULL,
-    rt           INT          NOT NULL,
-    rw           INT          NOT NULL,
-    village      VARCHAR(255) NOT NULL,
-    district     VARCHAR(255) NOT NULL,
-    city         VARCHAR(255) NOT NULL,
-    province     VARCHAR(255) NOT NULL,
-    postal_code  VARCHAR(5)   NOT NULL
+    id           VARCHAR(36),
+    street       VARCHAR(255),
+    house_number INT,
+    rt           INT,
+    rw           INT,
+    village      VARCHAR(255),
+    district     VARCHAR(255),
+    city         VARCHAR(255),
+    province     VARCHAR(255),
+    postal_code  VARCHAR(5)
 );
+
+ALTER TABLE sbd.address
+    ALTER id SET DEFAULT (UUID()),
+    MODIFY street VARCHAR(36) NOT NULL,
+    MODIFY house_number INT NOT NULL,
+    MODIFY rt INT NOT NULL,
+    MODIFY rw INT NOT NULL,
+    MODIFY village VARCHAR(255) NOT NULL,
+    MODIFY district VARCHAR(255) NOT NULL,
+    MODIFY city VARCHAR(255) NOT NULL,
+    MODIFY province VARCHAR(255) NOT NULL,
+    MODIFY postal_code VARCHAR(5) NOT NULL;
---
+++ Customer.sql
@@ -0,0 +1,4 @@
+CREATE TABLE sbd.Customer
+(
+    id VARCHAR(36) DEFAULT (UUID())
+);
