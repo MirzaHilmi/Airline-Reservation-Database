f3d6a46c31687d702c532abe773792f66db58339
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2023-04-20 05:16:36 +0700

    Restructure Query of Creation and Constraint

--- address-constraints.sql
+++ address-constraints.sql
@@ -1,18 +1,5 @@
-CREATE TABLE sbd.address
-(
-    id           VARCHAR(36) PRIMARY KEY,
-    street       VARCHAR(255),
-    house_number INT,
-    rt           INT,
-    rw           INT,
-    village      VARCHAR(255),
-    district     VARCHAR(255),
-    city         VARCHAR(255),
-    province     VARCHAR(255),
-    postal_code  VARCHAR(5)
-);
-
 ALTER TABLE sbd.address
+    ADD CONSTRAINT pk_address_id PRIMARY KEY (id),
     ALTER id SET DEFAULT (UUID()),
     MODIFY street VARCHAR(36) NOT NULL,
     MODIFY house_number INT NOT NULL,
@@ -23,3 +10,6 @@ ALTER TABLE sbd.address
     MODIFY city VARCHAR(255) NOT NULL,
     MODIFY province VARCHAR(255) NOT NULL,
     MODIFY postal_code VARCHAR(5) NOT NULL;
+
+ALTER TABLE sbd.address
+ALTER id SET DEFAULT (UUID());
---
+++ address.sql
@@ -0,0 +1,13 @@
+CREATE TABLE sbd.address
+(
+    id           VARCHAR(36),
+    street       VARCHAR(255),
+    house_number INT,
+    rt           INT,
+    rw           INT,
+    village      VARCHAR(255),
+    district     VARCHAR(255),
+    city         VARCHAR(255),
+    province     VARCHAR(255),
+    postal_code  VARCHAR(5)
+);
