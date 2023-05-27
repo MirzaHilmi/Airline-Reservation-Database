ec787b27a9c06c291aaade2de639dced5ffa5912
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2023-05-27 15:02:00 +0700

    `docs:` Update Relational Diagram

--- ddl/order-ticket-constraints.sql
+++ ddl/order-ticket-constraints.sql
@@ -5,20 +5,4 @@ ALTER TABLE sbd.order_ticket
     ADD CONSTRAINT pk_order_ticket_order_ticket_id PRIMARY KEY (order_ticket_id),
     ADD CONSTRAINT fk_order_ticket_customer_id FOREIGN KEY (customer_id) REFERENCES sbd.customer (customer_id) ON UPDATE CASCADE ON DELETE CASCADE;
 
-DELIMITER //
-
-CREATE TRIGGER update_order_price_amount
-AFTER INSERT ON sbd.ticket FOR EACH ROW
-BEGIN
-    UPDATE sbd.order_ticket
-    SET price_amount = (
-        SELECT SUM(price)
-        FROM sbd.ticket
-        WHERE order_ticket_id = NEW.order_ticket_id
-    )
-    WHERE order_ticket_id = NEW.order_ticket_id;
-END //
-
-DELIMITER ;
-
 DESCRIBE sbd.order_ticket;
--- ddl/payment-constraints.sql
+++ ddl/payment-constraints.sql
@@ -5,18 +5,4 @@ ALTER TABLE sbd.payment
     ADD CONSTRAINT pk_payment_payment_id PRIMARY KEY (payment_id),
     ADD CONSTRAINT fk_payment_order_ticket_id FOREIGN KEY (order_ticket_id) REFERENCES sbd.order_ticket (order_ticket_id) ON UPDATE CASCADE ON DELETE CASCADE;
 
-DELIMITER //
-
-CREATE TRIGGER update_payment_amount
-AFTER INSERT ON sbd.order_ticket
-FOR EACH ROW
-BEGIN
-    UPDATE sbd.payment
-    SET amount = NEW.price_amount
-    WHERE order_ticket_id = NEW.order_ticket_id;
-END //
-
-DELIMITER ;
-
-
 DESCRIBE sbd.payment;
