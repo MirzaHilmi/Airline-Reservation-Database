79bdeaf05db22267d464b28649ce77d2af9c904b
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2023-05-28 17:57:32 +0700

    `feat:` Introduce fk constraint for ticket table

--- ddl/ticket-constraints.sql
+++ ddl/ticket-constraints.sql
@@ -4,6 +4,7 @@ ALTER TABLE sbd.ticket
     ADD CONSTRAINT pk_ticket_ticket_id PRIMARY KEY (ticket_id),
     ADD CONSTRAINT fk_ticket_flight_id FOREIGN KEY (flight_id) REFERENCES sbd.flight (flight_id) ON UPDATE CASCADE ON DELETE CASCADE,
     ADD CONSTRAINT fk_ticket_seat_id FOREIGN KEY (seat_id) REFERENCES sbd.seat (seat_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    ADD CONSTRAINT fk_ticket_order_ticket_id FOREIGN KEY (order_ticket_id) REFERENCES sbd.order_ticket (order_ticket_id) ON UPDATE CASCADE ON DELETE CASCADE,
     ADD CONSTRAINT uq_ticket_seat_id UNIQUE (seat_id);
 
 DESCRIBE sbd.ticket;
