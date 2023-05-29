c610dabfaaa9841ec09aa703066646569f5ec774
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2023-05-29 18:00:31 +0700

    `feat:` Add TRIGGER for ticket reservation

---
+++ ddl/trigger/ticket.sql
@@ -0,0 +1,23 @@
+DROP TRIGGER IF EXISTS after_update_ticket;
+
+CREATE TRIGGER after_update_ticket
+    AFTER UPDATE
+    ON sbd.ticket
+    FOR EACH ROW
+
+BEGIN
+    SET @current_order_ticket_id = COALESCE(NEW.order_ticket_id, OLD.order_ticket_id);
+    UPDATE sbd.order_ticket
+    SET price_amount = (SELECT SUM(price)
+                        FROM sbd.ticket
+                        WHERE order_ticket_id = @current_order_ticket_id)
+    WHERE order_ticket_id = @current_order_ticket_id;
+
+    IF NEW.order_ticket_id IS NOT NULL AND NEW.order_ticket_id != OLD.order_ticket_id THEN
+        UPDATE sbd.order_ticket
+        SET price_amount = (SELECT SUM(price)
+                            FROM sbd.ticket
+                            WHERE order_ticket_id = OLD.order_ticket_id)
+        WHERE order_ticket_id = OLD.order_ticket_id;
+    END IF;
+END;
