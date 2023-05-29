7dabb5651664f14ceee56d9c1622b369d4eac395
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2023-05-29 21:30:54 +0700

    `feat:` Introduce new feature to check Airline's flight available ticket

---
+++ dql/flight-available-ticket.sql
@@ -0,0 +1,4 @@
+SELECT a.name, a.destination, a.departure, b.price
+FROM sbd.flight a
+INNER JOIN sbd.ticket b ON a.flight_id = b.flight_id
+WHERE b.seat_id = (SELECT seat_id FROM sbd.seat WHERE seat_id = b.seat_id AND !available);
